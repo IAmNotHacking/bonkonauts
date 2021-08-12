3b81ca611c83da403212b07233932bcd447933d1
IAmNotHacking <jessicad3141@gmail.com>
2021-08-12 15:42:29 -0500

    leaderboard name length fix

--- client/js/pages/leaderboard.js
+++ client/js/pages/leaderboard.js
@@ -10,6 +10,7 @@ function init() {
 
 		let count = 1;
 		for(let user of LEADERBOARD) {
+			if(user.name.length > 16) user.name = `${user.name.slice(0, 14)}...`;
 			leaderboard.innerHTML += `<li${user.name == window.user.username ? ' class="me"' : ''}><i ${count < 4 ? `class="${count == 1 ? 'gold' : count == 2 ? 'silver' : 'bronze'} fas fa-crown"` : ''}>${count > 3 ? count : ''}</i><span class="name">${user.name}</span><span class="xp">${user.xp.toLocaleString()}</span><span class="level">${xpToLevel(user.xp)}</span><span id="dbid">${user.id.toLocaleString()}</span></li>`
 			count++;
 		}
