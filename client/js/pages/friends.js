4f4f1041fba68107899abe13018b26978975908f
IAmNotHacking <jessicad3141@gmail.com>
2021-08-12 15:26:41 -0500

    sort friends by online/offline

--- client/js/pages/friends.js
+++ client/js/pages/friends.js
@@ -4,6 +4,9 @@ function init() {
 
 	var friendsContainer = document.querySelector('section.content');
 
+	const sortFriends = (friends) => friends.sort((a, b) => { if(a.roomid == null && b.roomid != null) return 1; else if(a.roomid != null && b.roomid == null) return -1; return 0; });  
+	FRIENDS = sortFriends(FRIENDS);
+
 	var friendListStr = "";
 	for(let friend of FRIENDS) {
 		friendListStr += `<li class="user"><span id="name">${friend.name}</span><span id="dbid">${friend.id.toLocaleString()}</span><span id="status" class="${friend.roomid ? "online" : "offline"}">${friend.roomid ? "Online" : "Offline"}</span></li>`;
